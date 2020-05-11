78afd3136d00cb0f45aaa086e5ea2e0e5f0f89b0
arturFormanchuk <4manchuk@gmail.com>
2020-05-11 22:29:50 +0300

    Commit

--- test.js
+++ test.js
@@ -5,8 +5,7 @@ function attempt(available, allowed, preferred) {
     for (const availableEl of available) {
       if (allowedEl === availableEl) {
         filtered.push(allowedEl);
-      }
-      else if (allowedEl === 'any') {
+      } else if (allowedEl === 'any') {
         filtered = [...available]
       }
     }
@@ -14,14 +13,12 @@ function attempt(available, allowed, preferred) {
   for (const preferredEl of preferred) {
     if (preferredEl === 'any') {
       result = [...filtered];
-    }
-    else if (filtered.filter(num => num === preferredEl)[0] === preferredEl) {
+    } else if (filtered.filter(num => num === preferredEl)[0] === preferredEl) {
       result.push(filtered.filter(num => num === preferredEl)[0]);
     } else if (filtered.length !== 0) {
       if (preferredEl < Math.max.apply(null, filtered)) {
         result.push(Math.max.apply(null, filtered))
       } else if (preferredEl >= Math.max.apply(null, filtered)) {
-        // result.push(Math.min.apply(null, filtered))
         const arr = filtered.filter(num => num !== preferredEl);
         result.push(arr[arr.length - 1])
       }
