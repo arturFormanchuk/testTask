e1f07b54ec98a9631ef7741b11677414db0e5fa4
arturFormanchuk <4manchuk@gmail.com>
2020-05-11 22:18:02 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,31 @@
+function attempt(available, allowed, preferred) {
+  let filtered = [];
+  let result = [];
+  for (const allowedEl of allowed) {
+    for (const availableEl of available) {
+      if (allowedEl === availableEl) {
+        filtered.push(allowedEl);
+      }
+      else if (allowedEl === 'any') {
+        filtered = [...available]
+      }
+    }
+  }
+  for (const preferredEl of preferred) {
+    if (preferredEl === 'any') {
+      result = [...filtered];
+    }
+    else if (filtered.filter(num => num === preferredEl)[0] === preferredEl) {
+      result.push(filtered.filter(num => num === preferredEl)[0]);
+    } else if (filtered.length !== 0) {
+      if (preferredEl < Math.max.apply(null, filtered)) {
+        result.push(Math.max.apply(null, filtered))
+      } else if (preferredEl >= Math.max.apply(null, filtered)) {
+        // result.push(Math.min.apply(null, filtered))
+        const arr = filtered.filter(num => num !== preferredEl);
+        result.push(arr[arr.length - 1])
+      }
+    }
+  }
+  return [...new Set(result)]
+}
